57e85bf2f69e97734bc49f80df8319ed81031df0
PraderioM <PraderioM@Gmail.com>
2019-11-12 13:16:09 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/main.js')
+def main_js():
+    return render_template('main.js')
+
+
+@app.route('/qrcode.js')
+def qrcode_js():
+    return render_template('qrcode.js')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
